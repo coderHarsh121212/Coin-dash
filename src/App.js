00b709d4cba38f4e1f28a0b99c92e5af6d0e5283
coderHarsh121212 <codesource287@gmail.com>
2024-02-12 12:47:14 +0530

    changes
    Signed-off-by: coderHarsh121212 <codesource287@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import CoinsNews from "./pages/CoinsNews";
+import HomePage from "./pages/HomePage";
+import Navbar from "./pages/Navbar";
+import ParticularPage from "./pages/particularPage";
+import TotalCoinPage from "./pages/TotalCoinPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<HomePage />}></Route>
+          <Route path="totalCoins" element={<TotalCoinPage />}></Route>
+          <Route path="coinNews" element={<CoinsNews />}></Route>
+          <Route path="particularPage/:id" element={<ParticularPage />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
